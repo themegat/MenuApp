7f6a0121bcd68746eb5f5283bd40725d7024b3fa
Tshepo Motsoeneng <tshepomotsoeneng0@gmail.com>
2018-07-06 16:32:57 +0200

    Commit 06-07-2018 16:32

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Menu } from 'ionic-angular/components/app/menu-interface';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
     selector: 'page-menu',
@@ -7,34 +9,41 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MenuPage {
     search_icon: any;
-    menu_list: any;
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+   public menu_list: Menu;
+private readonly url = "http://congos3.000webhostapp.com/menu.php?id=KFC";
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpClient) {
         this.search_icon = navParams.get('data');
-        this.menu_list = [{
-            name: "Brekkie Crunch Wrap",
-            desc: "A delicious spicy mini fillet (Crunch fillet) egg and cheese with grilled tomato sauce all wrapped up in a mini tortilla. Only available at selected KFC breakfast stores from 6am – 10:30am.",
-            price: "R 26,90"
-        },
-        {
-            name: "Small Chips",
-            desc: "A small portion of chips, crispy on the outside, soft and fluffy on the inside.",
-            price: "R 7,90"
-        },
-        {
-            name: "Coleslaw Regular",
-            desc: "A regular portion of freshly prepared cabbage onions and carrots in a delicious dressing.",
-            price: "R 14,90"
-        },
-        {
-            name: "Boxmaster Original",
-            desc: "No description",
-            price: "R 49,90"
-        },
-        {
-            name: "Mash Gravy Large",
-            desc: "A large portion of mash and delicious gravy.",
-            price: "R 14,90"
-        }]
+        this.http.get(this.url).subscribe(data =>{
+            console.log(data);
+        }, err =>{
+            console.log(err);
+        });
+        // this.menu_list = [{
+        //     name: "Brekkie Crunch Wrap",
+        //     desc: "A delicious spicy mini fillet (Crunch fillet) egg and cheese with grilled tomato sauce all wrapped up in a mini tortilla. Only available at selected KFC breakfast stores from 6am – 10:30am.",
+        //     price: "R 26,90"
+        // },
+        // {
+        //     name: "Small Chips",
+        //     desc: "A small portion of chips, crispy on the outside, soft and fluffy on the inside.",
+        //     price: "R 7,90"
+        // },
+        // {
+        //     name: "Coleslaw Regular",
+        //     desc: "A regular portion of freshly prepared cabbage onions and carrots in a delicious dressing.",
+        //     price: "R 14,90"
+        // },
+        // {
+        //     name: "Boxmaster Original",
+        //     desc: "No description",
+        //     price: "R 49,90"
+        // },
+        // {
+        //     name: "Mash Gravy Large",
+        //     desc: "A large portion of mash and delicious gravy.",
+        //     price: "R 14,90"
+        // }]
     }
 
 }
